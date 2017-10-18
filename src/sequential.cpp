afd16ee40d4e3f84d402465810805bc514f9f0f8
BrianJConway <brianc@nevada.unr.edu>
2017-10-17 18:23:21 -0700

    added better documentation

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -32,11 +32,14 @@ int main()
 
 void bellmanFord(int source, int graph[][VERTICES], int distances[], int predecessors[])
 {
+  // initialize function/variables
   queue<int> vertexQueue;
   int index, currentVertex, newDist;
 
+  // Enqueue source node to vertex queue
   vertexQueue.push(source);
 
+  // Initialize distance and predecessor matrices
   for (index = 0; index < VERTICES; index++)
   {
     if(source != index)
@@ -51,21 +54,30 @@ void bellmanFord(int source, int graph[][VERTICES], int distances[], int predece
     predecessors[index] = -1;
   }
 
+  // Loop while vertex queue is not empty
   while (!vertexQueue.empty())
   {
+    // Dequeue current vertex
     currentVertex = vertexQueue.front();
     vertexQueue.pop();
 
+    // Loop through all vertices
     for (index = 0; index < VERTICES; index++)
     {
+      // Check if current vertex is a neighbor 
       if (graph[currentVertex][index] != INF)
       {
+        // Calculate distance to vertex at this index if path is taken through current vertex
         newDist = distances[currentVertex] + graph[currentVertex][index];
 
+        // Check if path through current vertex is smaller than current optimal path
         if (newDist < distances[index])
         {
+          // Update arrays with more optimal path
           distances[index] = newDist;
           predecessors[index] = currentVertex;
+
+          // Push vertex at this index onto queue
           vertexQueue.push(index);
         }
       }
